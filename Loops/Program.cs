1e7a3e2fc352a29b9d9848bd9fc8445f8a066b44
Martin Sillaste <martin.sillaste@tktk.ee>
2024-10-19 15:01:52 +0300

    add loops code

---
+++ Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+//FOR-LOOP
+//DRY-principle - do not repeat yourself
+
+for(int i = 0; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We don't need no education!");
+}
+
+Console.WriteLine("Have a nice day.");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+Console.WriteLine("Guess the number");
+
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+
+    Console.WriteLine("Guess the number between 1-3");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("YAY, YOU WON!");
+
+        loopActive = false;
+        // saab ka kasutada break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong number ;( . Try again!");
+    }
+}
+
+Console.WriteLine("See you next time!");
